84756d6c4b3c400bd2b06cc3a5ceffbb78b189eb
Jose Luis Rodriguez Choque <jlrodriguez.codecofe@gmail.com>
2015-12-16 19:03:56 +0100

    Javascript codigo servidor
    Un ejemplo de como implemetar la interface ICallbackEventHandler para
    interactuar los eventos del cliente hacia el código servidor en este
    caso C#.

---
+++ InteraccionJavascriptCodigoservidor/InteraccionJavascriptCodigoservidor/WebEjemplo.aspx
@@ -0,0 +1,83 @@
+﻿<%--
+
+Codecofe.com
+
+Email: contact@codecofe.com
+Skype: jlrodriguez.codecofe
+
+--%>
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebEjemplo.aspx.cs" Inherits="InteraccionJavascriptCodigoservidor.WebEjemplo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Ejemplo Interacci&oacute;n JavaScript y C&oacute;digo Servidor</title>
+
+    <%--referencia jquery--%>
+    <script src="http://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+
+
+    <script type="text/javascript">
+        // Variables globales.
+        var Numero1, Numero2;
+
+        function Sumar() {
+            ProcesarDatos('Sumar');
+        }
+
+        function Multiplicar() {
+            ProcesarDatos('Multiplicar');
+        }
+
+        // Función obtiene datos de controles y asigna parametros.
+        function ProcesarDatos(comando) {
+
+            Numero1 = $('#txtPrimerNumero').val();
+            Numero2 = $('#txtSegundoNumero').val();
+
+            var parametros = { Comando: comando, Valor1: Numero1, Valor2: Numero2 };
+
+            // Convierte json a cadena.
+            // Envia parametros al método RaiseCallbackEvent de C#.
+            UsaCallback(JSON.stringify(parametros));
+        }
+
+        // Función que obtiene respuesta del método RaiseCallbackEvent por medio del parametro _callbackResult.
+        function HandleEvent(result, context) {
+
+            $('#dvResultado').html('');
+
+            // Verifica si resultado no es vacio.
+            if (result != '') {
+                $('#dvResultado').html('<span><strong> EL RESULTADO ES ' + result + ' </strong></span>')
+            }
+        }
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Ejemplo javascript y c&oacute;digo servidor</h1>
+            <br />
+            <br />
+            Primer N&uacute;mero:
+           <input type="text" id="txtPrimerNumero" />
+            <br />
+            <br />
+            Segundo N&uacute;mero:
+            <input type="text" id="txtSegundoNumero" />
+            <br />
+            <br />
+            <div id="dvResultado"></div>
+            <br />
+            <br />
+            <input type="button" value="Sumar" onclick="Sumar()" />&nbsp;&nbsp;
+            <input type="button" value="Multiplicar" onclick="Multiplicar()" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ InteraccionJavascriptCodigoservidor/InteraccionJavascriptCodigoservidor/WebEjemplo.aspx.cs
@@ -0,0 +1,89 @@
+﻿/*
+* Codecofe.com
+*
+* Email: contact@codecofe.com
+* Skype: jlrodriguez.codecofe
+* 
+*/
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+
+namespace InteraccionJavascriptCodigoservidor
+{
+    // Se aplica herencia de ICallbackEventHandler a clase para aplicar comportamiento.
+    public partial class WebEjemplo : System.Web.UI.Page, System.Web.UI.ICallbackEventHandler
+    {
+        // Variable de respuesta para javascript.
+        private string _callbackResult = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // - Arg - Argumento pasado desde el cliente al servidor.
+            // - HandleEvent - Nombre del controlador de eventos del lado del cliente que recibe el resultado del evento de servidor.
+            // - Contexto - script del lado del cliente que se evalúa en el cliente antes de la inicialización.
+            string callbackReference = Page.ClientScript.GetCallbackEventReference(
+                this, "arg", "HandleEvent", "context");
+
+            // UsaCallback es el nombre del evento que se invocara del lado del cliente. 
+            // El parametro arg envia valores al evento servidor.
+            string callbackScript = "function UsaCallback(arg, context) {" + callbackReference + ";}";
+
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "UsaCallback", callbackScript, true);
+        }
+
+        /// <summary>
+        /// Evento de servidor.
+        /// </summary>
+        /// <param name="eventArg">Parametros enviados del lado de cliente.</param>
+        public void RaiseCallbackEvent(string eventArg)
+        {
+            decimal vResultado = 0;
+
+            // Convierte cadena de formato json a diccionario clave valor.
+            Dictionary<string, string> vValores = JsonConvert.DeserializeObject<Dictionary<string, string>>(eventArg);
+
+            if (vValores.Any())
+            {
+                // Obtiene valor de comando de lista de valores.
+                string vComando = vValores["Comando"];
+
+                decimal vNumero1;
+                decimal vNumero2;
+
+                decimal.TryParse(vValores["Valor1"], out vNumero1);
+                decimal.TryParse(vValores["Valor2"], out vNumero2);
+
+                // Selecciona el tipo de comando.
+                switch (vComando)
+                {
+                    case "Sumar":
+                        vResultado = vNumero1 + vNumero2;
+                        break;
+                    case "Multiplicar":
+                        vResultado = vNumero1 * vNumero2;
+                        break;
+                    // Otros eventos ...
+                    default:
+                        break;
+                }
+            }
+
+            // Asigna resultado de proceso.
+            _callbackResult = vResultado.ToString();
+
+        }
+
+        /// <summary>
+        /// Metodo heredado de ICallbackEventHandler.
+        /// </summary>
+        /// <returns>retona valor de callback.</returns>
+        public string GetCallbackResult()
+        {
+            return _callbackResult;
+        }
+    }
+}
